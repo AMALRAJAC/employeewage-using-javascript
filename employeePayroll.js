87c8cbdc2b6126aefee2d46d7d1eabc643414bf4
unknown <acamalraj@gmail.com>
2021-05-24 10:09:54 +0530

    add program for store daily wage into array

--- employeePayroll.js
+++ employeePayroll.js
@@ -1,24 +1,8 @@
-//constants
-let empHrs=0;
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
-const MAX_HRS_IN_MONTH=100;
-const NUM_OF_WORKING_DAYS=10;
-let totalEmpHrs=0;
-let totalWorkingDays=0;
-//calculate employee hours when max days or max hours reached
-while(totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS){
-    totalWorkingDays++;
-    let empCheck=Math.floor(Math.random()*10)%3;
-    totalEmpHrs+=getWorkingHours(empCheck);
+//function to calculate daily wage
+function calcDailyWage(empHrs){
+    return empHrs*WAGE_PER_HOUR;
 }
-//calculate employee wage for a month
-let empWage=totalEmpHrs*WAGE_PER_HOUR;
-console.log("EMP_WAGE="+empWage);
-//function to get employee hours
+//function to get employee working hours
 function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -29,3 +13,27 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+//constants
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const MAX_HRS_IN_MONTH =160;
+const NUM_OF_WORKING_DAYS=20;
+const WAGE_PER_HOUR=20;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArray=new Array();
+//get total working hours by month or max working hours and store employee hours into array
+while (totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    //get random values range fron 0-2
+    let empCheck=Math.floor(Math.random()*10)%3;
+    let empHrs=getWorkingHours(empCheck);
+    totalEmpHrs+=empHrs;
+    //store daily wage into array
+    empDailyWageArray.push(calcDailyWage(empHrs));
+}
+//calculate employee wage
+let empWage=calcDailyWage(totalEmpHrs);
+console.log("total emp hours="+totalEmpHrs);
