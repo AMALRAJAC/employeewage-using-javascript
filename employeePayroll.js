d3087c7fc9e463756c8a31d06780f0fa69965b10
unknown <acamalraj@gmail.com>
2021-05-24 10:00:12 +0530

    add program for calculate employee wage if total days or hours reached

--- employeePayroll.js
+++ employeePayroll.js
@@ -1,18 +1,22 @@
 //constants
-const NUM_OF_WORKING_DAYS=2;
 let empHrs=0;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-//calculate total employee hours for a month
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS=10;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+//calculate employee hours when max days or max hours reached
+while(totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck=Math.floor(Math.random()*10)%3;
-    empHrs+=getWorkingHours(empCheck)
+    totalEmpHrs+=getWorkingHours(empCheck);
 }
-//calclute the employee wage
-let empWage=empHrs*WAGE_PER_HOUR;
+//calculate employee wage for a month
+let empWage=totalEmpHrs*WAGE_PER_HOUR;
 console.log("EMP_WAGE="+empWage);
 //function to get employee hours
 function getWorkingHours(empCheck){
