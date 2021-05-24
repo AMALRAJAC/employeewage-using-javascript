bc71fcf2612c2af108913338525a44963db90a91
unknown <acamalraj@gmail.com>
2021-05-24 09:55:36 +0530

    add program to get employee wage for a month

--- employeePayroll.js
+++ employeePayroll.js
@@ -1,10 +1,20 @@
 //constants
+const NUM_OF_WORKING_DAYS=2;
+let empHrs=0;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-//function it returns employee hours
+//calculate total employee hours for a month
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck)
+}
+//calclute the employee wage
+let empWage=empHrs*WAGE_PER_HOUR;
+console.log("EMP_WAGE="+empWage);
+//function to get employee hours
 function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -15,11 +25,3 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empHrs=0;
-//take random value ranges from 0-2
-let empCheck=Math.floor(Math.random()*10)%3;
-//function call
-empHrs=getWorkingHours(empCheck);
-//calculate employee wage
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("EMP_WAGE="+empWage);
